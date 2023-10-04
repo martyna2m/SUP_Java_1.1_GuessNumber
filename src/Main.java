1920a214baa8076f4a9afd7b04397b7fffdccf93
martyna2m <bsom2m@gmail.com>
2023-10-04 19:52:57 +0200

    Invoke the Game in the main Class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
-import java.util.Random;
-
 public class Main {
+    public static void main(String[] args) {
+
+
+        NumberGame gameRound = new NumberGame();
+
+        try {
+            gameRound.guessTheNumberGame();
+        } catch (Exception e) {
+            System.out.println("Your guess must be an integer from 0 to 99! Game over, start again.");
+        }
+    }
+
 
 }
+
+
+
+
+
+
